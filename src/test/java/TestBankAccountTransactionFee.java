cdde0a3e9f44985d7b4e7bf01a15dc98492e3cdd
Derrick Park <derrick8689@gmail.com>
2019-02-27 20:12:54 -0800

    Initial Commit

---
+++ src/test/java/TestBankAccountTransactionFee.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class TestBankAccountTransactionFee {
+    @Parameterized.Parameters(name = "{index}: @BankAccount.transactionFee()")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {5.00, true},
+                {7.00, true},
+                {10.00, false},
+        });
+    }
+
+    @Parameterized.Parameter
+    public double inputs;
+
+    @Parameterized.Parameter(1)
+    public boolean expected;
+
+    @Test
+    public void testTransactionFee() {
+        BankAccount account = new BankAccount();
+        account.deposit(10.00);
+        account.deposit(50.00);
+        account.deposit(10.00);
+        account.deposit(70.00);
+
+        Assert.assertEquals(expected, account.transactionFee(inputs));
+    }
+}
