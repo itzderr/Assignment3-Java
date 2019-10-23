73723c959ed58c14dc1bf0d4b3f79a593ca8f426
ricktang1991 <55897267+ricktang1991@users.noreply.github.com>
2019-10-22 18:58:53 -0700

    fixeded

---
+++ Assignment3-Java/src/test/java/TestBankAccountTransactionFee.java
@@ -0,0 +1,37 @@
+import ca.ciccc.BankAccount;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class TestBankAccountTransactionFee {
+    @Parameterized.Parameters(name = "{index}: @ca.ciccc.BankAccount.transactionFee()")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {5.00, true},
+                {7.00, true},
+                {20.00, false},
+        });
+    }
+
+    @Parameterized.Parameter
+    public double inputs;
+
+    @Parameterized.Parameter(1)
+    public boolean expected;
+
+    @Test
+    public void testTransactionFee() {
+        BankAccount account = new BankAccount();
+        account.deposit(10.00);
+        account.deposit(50.00);
+        account.deposit(10.00);
+        account.deposit(70.00);
+
+        Assert.assertEquals(expected, account.transactionFee(inputs));
+    }
+}
