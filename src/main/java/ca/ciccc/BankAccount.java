871d7093434082f80c23c935a030b66585bd373a
yk-jp <k82.y12@gmail.com>
2022-02-28 15:51:26 -0800

    complete assignment3

--- src/main/java/ca/ciccc/BankAccount.java
+++ src/main/java/ca/ciccc/BankAccount.java
@@ -11,7 +11,8 @@ public class BankAccount {
     private int transactionCount = 0;
     private String transactionText = "";
 
-    public BankAccount() { }
+    public BankAccount() {
+    }
 
     public BankAccount(double balance) {
         this.id = "test account";
@@ -51,6 +52,10 @@ public class BankAccount {
         return transactionText;
     }
 
+    public void setTransactionCount(int transactionCount) {
+        this.transactionCount = transactionCount;
+    }
+
     public void deposit(double amount) {
         if (amount >= 0) {
             balance += amount;
@@ -75,22 +80,22 @@ public class BankAccount {
      * fees with greater than $0.00 remaining, the method will return {@code true}. If the balance cannot
      * afford all of the fees or has no money left, the balance is left as 0.0 and method returns
      * {@code false}.
-     *
+     * <p>
      * For example, given the following ca.ciccc.BankAccount object.
-     *
+     * <p>
      * ca.ciccc.BankAccount savings = new ca.ciccc.BankAccount("Jimmy");
      * savings.deposit(10.00);
      * savings.deposit(50.00);
      * savings.deposit(10.00);
      * savings.deposit(70.00);
-     *
+     * <p>
      * savings.transactionFee(5.00);
-     *
+     * <p>
      * The account would be deducted $5 + $10 + $15 + $20 for the four transactions, leaving a final
      * balance of $90.00. The method would return true.
-     *
+     * <p>
      * savings.transactionFee(10.00);
-     *
+     * <p>
      * Then the account would be deducted $10 + $20 + $30 + $40 for the four transactions, leaving
      * a final balance of $0.00. The method would return false.
      *
@@ -100,7 +105,12 @@ public class BankAccount {
     public boolean transactionFee(double fee) {
         // TODO 2: Your code goes here.
 
-        return false;
+        double finalBalance = this.getBalance();
+        for (int i = 0; i < this.getTransactionCount(); i++) {
+            finalBalance -= (i + 1) * fee;
+        }
+
+        return finalBalance > 0;
     }
 
     /**
@@ -109,11 +119,11 @@ public class BankAccount {
      * the money.
      * There is a $5.00 fee for transferring money, so this much must be deducted from the current
      * account's balance before any transfer.
-     *
+     * <p>
      * If `this` account object does not have enough money to make full transfer, then transfer
      * whatever money is left after the $5.00 fee is deducted. If this account has under $5.00 or amount is 0 or less,
      * no transfer should occur and neither account's state should be modified.
-     *
+     * <p>
      * If any amount of money is transferred, return {@code true}. Otherwise {@code false}.
      *
      * @param amount
@@ -122,25 +132,40 @@ public class BankAccount {
      */
     public boolean transfer(double amount, BankAccount other) {
         // TODO 3: Your code goes here.
+        double serviceFee = 5;
+        double currBalance = this.getBalance();
+
+        if (currBalance > amount + serviceFee) {
+            this.withdraw(amount + serviceFee);
+            other.deposit(amount);
+
+            return true;
+        } else if (currBalance >= serviceFee) {
+            this.withdraw(currBalance);
+            other.deposit(currBalance - serviceFee);
+            return true;
+        }
 
         return false;
     }
 
-    /**
-     * Your {@code toString()} method should return a string that contains the account's name
-     * and balance separated by a comma and space. For example, if an account object has name
-     * "Derrick" and a balance of 20.55, this method should return "Derrick, $20.55"
-     * There are some special cases you should handle. If the balance is negative, put the -sign
-     * before the dollar sign. Also, always display the cents as a two-digit number.
-     * For example, if the same object had a balance of -17.5, your method should return
-     * "Derrick, -$17.50"
-     *
-     * @return string representation of ca.ciccc.BankAccount
-     */
-    @Override
-    public String toString() {
-        // TODO 1: Your code goes here.
-
-        return "";
+        /**
+         * Your {@code toString()} method should return a string that contains the account's name
+         * and balance separated by a comma and space. For example, if an account object has name
+         * "Derrick" and a balance of 20.55, this method should return "Derrick, $20.55"
+         * There are some special cases you should handle. If the balance is negative, put the -sign
+         * before the dollar sign. Also, always display the cents as a two-digit number.
+         * For example, if the same object had a balance of -17.5, your method should return
+         * "Derrick, -$17.50"
+         *
+         * @return string representation of ca.ciccc.BankAccount
+         */
+        @Override
+        public String toString () {
+            // TODO 1: Your code goes here.
+            String balance = this.getBalance() > 0 ? "" : "-";
+            balance += "$" + String.format("%.2f", Math.abs(this.getBalance()));
+            String name = this.getName();
+            return name + ", " + balance;
+        }
     }
-}
